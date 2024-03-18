05256a6ad089c8b21f0c5eace9ee4b373d1cb5d1
Isaac Munene Ndereba <38168136+munenendereba@users.noreply.github.com>
2024-03-18 14:13:32 +0300

    added tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module munenendereba/leetcode-gc-div-str
+
+go 1.22.0
---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var tests = []struct {
+	string1 string
+	string2 string
+	divisor string
+}{
+	{"ABCABC", "ABC", "ABC"},
+	{"ABABAB", "ABAB", "AB"},
+	{"LEET", "CODE", ""},
+}
+
+func TestGcdOfStrings(t *testing.T) {
+	for i, test := range tests {
+		testname := fmt.Sprintf("running test %d", i)
+
+		t.Run(testname, func(t *testing.T) {
+			div := gcdOfStrings(test.string1, test.string2)
+
+			if div != test.divisor {
+				t.Errorf("gcdOfStrings(%s,%s) expected %s but got %s", test.string1, test.string2, test.divisor, div)
+			}
+		})
+	}
+
+}
